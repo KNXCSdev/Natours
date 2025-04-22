47ad4cd747f7c41ad1b062dbb0afe8d9b7773c30
KNXCSdev <michal.miksho@gmail.com>
2025-04-22 17:01:33 +0200

    PATCH,DELETE,Route for method chaining

--- app.js
+++ app.js
@@ -21,21 +21,31 @@ const toursData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: toursData.length,
     data: { tours: toursData },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
+const getTour = (req, res) => {
+  const tour = toursData.find((el) => el.id === Number(req.params.id));
 
+  if (!tour)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
   const newId = toursData[toursData.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-
-  console.log(newTour);
 
   toursData.push(newTour); //PUSH BECAUSE WE NEED TO PASS IT TO WRITE FILE
 
@@ -49,10 +59,45 @@ app.post('/api/v1/tours', (req, res) => {
         data: {
           tour: newTour,
         },
-      }); //CREATED
+      }); //201 means CREATED
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > toursData.length)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here...',
+    },
+  }); //OK WHEN WE UPDATE
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > toursData.length)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  }); //204 NO CONTENT WHEN WE DELETE
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour); //CHAINING METHODS
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
