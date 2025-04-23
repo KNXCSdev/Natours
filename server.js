8d4b8ef4425f9d2979dab37aaf4c910024d37226
KNXCSdev <michal.miksho@gmail.com>
2025-04-23 23:55:54 +0200

    Mongoose connecting to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 /* eslint-disable no-undef */
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB CONNECTION SUCCESSFULL');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
