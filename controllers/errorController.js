e82b8baa8466e29d66e011e62a8bf17ac21e5bca
KNXCSdev <michal.miksho@gmail.com>
2025-04-27 17:12:46 +0200

    Error handling 1/3

---
+++ controllers/errorController.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
