7f383a40e88b9d98bcf90c6ca1aedb17e9afb3e5
KNXCSdev <michal.miksho@gmail.com>
2025-05-13 20:40:44 +0200

    Tour page,Tour Overview

---
+++ controllers/viewsController.js
@@ -0,0 +1,29 @@
+const Review = require('../models/reviewModel');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find({ slug: req.params.slug });
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    reviews,
+    tour,
+  });
+});
