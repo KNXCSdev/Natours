79fe541facc337b6b0b8ab7051c33d94d532d8ee
KNXCSdev <michal.miksho@gmail.com>
2025-05-14 18:09:25 +0200

    logging in

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+import 'core-js';
+import 'regenerator-runtime/runtime';
+import { displayMap } from './mapbox';
+import { login } from './login';
+import { showAlert } from './alerts';
+
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    login(email, password);
+  });
+}
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
