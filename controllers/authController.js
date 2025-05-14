00dc2ff4d13d74f84990d322a9dd3a2205ed2021
KNXCSdev <michal.miksho@gmail.com>
2025-05-14 21:16:16 +0200

    logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -207,27 +207,43 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
+
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET,
-    );
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET,
+      );
 
-    // 3) Check if user still exist
-    const freshUser = await User.findById(decoded.id);
-    if (!freshUser) return next();
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
 
-    // 4) Check if user changed password after the token was issued
-    if (freshUser.changedPasswordAfter(decoded.iat)) return next();
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
 
-    //THERE IS A LOGGED IN USER
-    //GIVES ACCESS TO THE TEMPLATE ENGINE
-    res.locals.user = freshUser;
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
   }
   next();
-});
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
