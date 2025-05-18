677597fa27817dc3469540fa841be2ba0b162805
Michał <michal.miksho@gmail.com>
2025-05-18 22:05:18 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,7 +32,6 @@ const upload = multer({
 exports.uploadUserPhoto = upload.single('photo');
 
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
-  console.log('Received file:', req.file);
 
   if (!req.file) return next();
 
